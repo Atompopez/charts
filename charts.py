a9593177e2b7de02d03772116f7c5012b3f4309b
Atompopez <johanandrey0210@gmail.com>
2023-03-02 01:07:40 -0500

    create main.py

--- charts.py
+++ charts.py
@@ -1,1 +1,10 @@
-print("hello desde mi maquina")
+import matplotlib.pyplot as plt
+
+def generate_pie_chart():
+    labels = ['A','B','C']
+    values = [150,200,300]
+
+    fig, ax = plt.subplots()
+    ax.pie(values,labels=labels)
+    plt.savefig('pie.png')
+    plt.close()
